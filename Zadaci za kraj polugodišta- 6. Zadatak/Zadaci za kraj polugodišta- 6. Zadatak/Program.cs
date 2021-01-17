b4433c6b6caab3610bd2b781f19be57a6a1973dd
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-01-17 15:26:36 +0100

    Add files via upload

---
+++ Zadaci za kraj polugodišta- 6. Zadatak/Zadaci za kraj polugodišta- 6. Zadatak/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaci_za_kraj_polugodišta__6.Zadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0, x=0;
+            
+            for (int i = 0; i <= n; i++)
+            {
+
+                x = x + i;
+
+            }
+            Console.WriteLine("Zbroj prvih ");
+
+            Console.WriteLine(n);
+
+            Console.WriteLine(" brojeva je ");
+
+            Console.WriteLine(x);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
